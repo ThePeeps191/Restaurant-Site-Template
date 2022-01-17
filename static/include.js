c436b47ad055776ebd89204454e1aad89c6292d0
The Peeps191 <thepeeps.wang@gmail.com>
2022-01-16 21:01:08 -0700

    setup site js

---
+++ static/include.js
@@ -0,0 +1,10 @@
+includes = `
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/static/style.css">
+<script src="/static/script.js"></script>
+<script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"></script>
+<link rel="icon" type="image/x-icon" href="/static/logo.png">
+`;
+
+document.write(includes);
